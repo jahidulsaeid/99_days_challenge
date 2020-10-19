43197a4cef2c6ec95ffb533d2d66b877d29ea018
jahidulsaeid <jahdiul.saeid@gmail.com>
2020-10-20 01:50:13 +0600

    add completed

--- 1.1_todo_app/src/components/TodoList.js
+++ 1.1_todo_app/src/components/TodoList.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Todo from "./Todo";
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, setTodos }) => {
   return (
     <div className="todo-container">
       <ul className="todo-list">
         {todos.map((todo) => (
-          <Todo todos={todos} setTodos={setTodos} text={todo.text} key={todo.id}/>
+          <Todo
+            todos={todos}
+            todo={todo}
+            setTodos={setTodos}
+            text={todo.text}
+            key={todo.id}
+          />
         ))}
       </ul>
     </div>
